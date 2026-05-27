d442f75be1b5b0946ff8704d5865d7fdb8d30830
dev62767-beep <dev62767@gmail.com>
2026-05-27 21:01:56 +0530

    Update interview.api.js

--- Frontend/src/features/interview/services/interview.api.js
+++ Frontend/src/features/interview/services/interview.api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: "https://resume-parser-gen-ai.onrender.com/",
+    baseURL: "https://resume-parser-gen-ai.onrender.com",
     withCredentials: true,
 })
 
