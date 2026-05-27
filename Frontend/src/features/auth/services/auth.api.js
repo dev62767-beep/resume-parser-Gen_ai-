8c26ce63276f4e655e6699173c315bfc4e983ec8
dev62767-beep <dev62767@gmail.com>
2026-05-27 20:51:34 +0530

    Change API base URL to production endpoint

--- Frontend/src/features/auth/services/auth.api.js
+++ Frontend/src/features/auth/services/auth.api.js
@@ -2,7 +2,7 @@ import axios from "axios"
 
 
 const api = axios.create({
-    baseURL: "http://localhost:3000",
+    baseURL: "https://resume-parser-gen-ai.onrender.com/",
     withCredentials: true
 })
 
