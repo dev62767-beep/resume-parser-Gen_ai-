d43021354f091f98653eafbc7390763ce8a3a32c
dev62767-beep <dev62767@gmail.com>
2026-05-29 19:25:10 +0530

    Delete server.js

--- server.js
+++
@@ -1,9 +0,0 @@
-const app = require('./src/app');
-const connectDB = require('./src/db/db');
-const { generateInterviewReport } = require('./src/services/ai.services');
-
-connectDB();
-
-app.listen(3000, () => {
-  console.log('Server is running on port 3000');
-});
