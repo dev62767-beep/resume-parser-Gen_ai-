e7df7aa528b60fda6bef96e0b5a690565bdb3dfb
dev62767-beep <dev62767@gmail.com>
2026-05-29 19:24:00 +0530

    Delete src directory

--- src/middleware/file.middleware.js
+++
@@ -1,8 +0,0 @@
-const multer = require('multer');
-const Upload = multer({
-    storage: multer.memoryStorage(),
-    limits  : {
-        fileSize: 10 * 1024 * 1024
-    }
-})  
-module.exports = {Upload}
--- src/models/blacklist.model.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-const blacklistSchema = new mongoose.Schema({
-    token: { type: String, required: [true, "token is required to add to blacklist"] },
-   
-},
-{    timestamps: true }
-);
-const Blacklist = mongoose.model('Blacklist', blacklistSchema);
-module.exports = Blacklist; 
